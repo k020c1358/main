847f06ab63c0f4949b7f37de5f19ff379a07adda
k020c1358 <k020c1358@it-neec.jp>
2020-12-02 07:06:49 +0000

    授業の練習

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ image_list=["n01.jpg","n02.jpg","n03.jpg"]
 images_1="n03.jpg"
 @app.route('/')
 def index():
-  
+  disabled="disabled"
   return render_template('index.html',
                            image_list=image_list,
-                           images_1=images_1)
+                           images_1=images_1,
+                           disabled=disabled)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
